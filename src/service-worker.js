99f82f07d9a66f34a22535942238ee0048c8e3a1
Khangg <le.khang@horus.com.vn>
2022-02-17 17:46:07 +0700

    importScripts

--- src/service-worker.js
+++ src/service-worker.js
@@ -14,7 +14,7 @@ import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
 
-self.importScripts('/localforage-1.10.0.min.js');
+importScripts('/localforage-1.10.0.min.js');
 
 clientsClaim();
 
